58ae3d8d68869a76cecd5f9ca13bf00b8230614a
victorFernandez173 <victor.fer.gor@proton.me>
2024-12-22 13:30:51 +0100

    preparado bloque de rutas con middlewares auth y roles

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,31 @@
 <?php
 
+use App\Http\Controllers\EncuestasController;
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return Inertia::render('Welcome');
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+
+    Route::middleware('role:admin|employee')->group(function () {
+        Route::get('encuestas', [EncuestasController::class, 'index'])->name('encuestas.index');
+        Route::get('encuestas/{id}', [EncuestasController::class, 'show'])->name('encuestas.show');
+        Route::post('encuestas', [EncuestasController::class, 'store'])->name('encuestas.store');
+    });
+
+    Route::middleware('role:admin')->group(function () {
+        Route::put('encuestas/{id}', [EncuestasController::class, 'update'])->name('encuestas.update');
+        Route::patch('encuestas/{id}', [EncuestasController::class, 'update'])->name('encuestas.update');
+        Route::delete('encuestas/{id}', [EncuestasController::class, 'destroy'])->name('encuestas.destroy');
+    });
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
