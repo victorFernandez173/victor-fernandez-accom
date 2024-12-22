a638129b9f03392e906295a096e722946368fc3a
victorFernandez173 <victor.fer.gor@proton.me>
2024-12-22 23:11:06 +0100

    redireccion login

--- routes/auth.php
+++ routes/auth.php
@@ -4,8 +4,9 @@ use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])
-        ->name('login');
+    Route::get('login', function () {
+        return redirect('/');
+    })->name('login');
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
 });
 
