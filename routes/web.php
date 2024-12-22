38358839d293cf8584fb6c2369ce9444c5833539
victorFernandez173 <victor.fer.gor@proton.me>
2024-12-22 18:33:46 +0100

    reorganización rutas para el uso desde el front de la propia app

--- routes/web.php
+++ routes/web.php
@@ -10,21 +10,27 @@ Route::get('/', function () {
 });
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-});
-
-Route::middleware(['auth', 'verified'])->group(function () {
     Route::middleware('role:admin|employee')->group(function () {
-        Route::get('encuestas', [EncuestasController::class, 'index'])->name('encuestas.index');
-        Route::get('encuestas/{id}', [EncuestasController::class, 'show'])->name('encuestas.show');
-        Route::post('encuestas', [EncuestasController::class, 'store'])->name('encuestas.store');
+        Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     });
 
-    Route::middleware('role:admin')->group(function () {
-        Route::put('encuestas/{id}', [EncuestasController::class, 'update'])->name('encuestas.update');
-        Route::patch('encuestas/{id}', [EncuestasController::class, 'update'])->name('encuestas.update');
-        Route::delete('encuestas/{id}', [EncuestasController::class, 'destroy'])->name('encuestas.destroy');
+    Route::middleware('role:admin')->group(function () {Route::delete('/dashboard/encuestas/{id}', [DashboardController::class, 'delete'])->name('dashboard.delete');
     });
 });
+
+//API
+//Route::middleware(['auth', 'verified'])->group(function () {
+//    Route::middleware('role:admin|employee')->group(function () {
+//        Route::get('encuestas', [EncuestasController::class, 'index'])->name('encuestas.index');
+//        Route::get('encuestas/{id}', [EncuestasController::class, 'show'])->name('encuestas.show');
+//        Route::post('encuestas', [EncuestasController::class, 'store'])->name('encuestas.store');
+//    });
+//
+//    Route::middleware('role:admin')->group(function () {
+//        Route::put('encuestas/{id}', [EncuestasController::class, 'update'])->name('encuestas.update');
+//        Route::patch('encuestas/{id}', [EncuestasController::class, 'update'])->name('encuestas.update');
+//        Route::delete('encuestas/{id}', [EncuestasController::class, 'destroy'])->name('encuestas.destroy');
+//    });
+//});
 
 require __DIR__.'/auth.php';
