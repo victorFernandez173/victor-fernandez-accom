2de529c865888bf9930bcf719d1418087fd078fe
victorFernandez173 <victor.fer.gor@proton.me>
2024-12-22 13:48:47 +0100

    eliminadas rutas innecesarias para el proyecto

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\EncuestasController;
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -25,12 +24,6 @@ Route::middleware(['auth', 'verified'])->group(function () {
         Route::patch('encuestas/{id}', [EncuestasController::class, 'update'])->name('encuestas.update');
         Route::delete('encuestas/{id}', [EncuestasController::class, 'destroy'])->name('encuestas.destroy');
     });
-});
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
