5b30e0a1fcd0800fa3399d7cab176dd5f257d6f7
victorFernandez173 <victor.fer.gor@proton.me>
2024-12-22 22:48:10 +0100

    reagrupación bloques formulario

--- resources/js/Components/FormularioEncuesta.jsx
+++ resources/js/Components/FormularioEncuesta.jsx
@@ -113,96 +113,104 @@ const FormularioEncuesta = () => {
                 </div>
             </div>
             {/* Subproductos */}
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-                {/* Subproducto Luz Field */}
-                {(producto === "LUZ" || producto === "GAS" || producto === "DUAL") && (
-                    <div>
-                        <label htmlFor="subproducto" className="block text-sm font-medium text-gray-700">
-                            Subproducto
-                        </label>
-                        <select
-                            id="subproducto"
-                            value={subproducto}
-                            onChange={(e) => setSubproducto(e.target.value)}
-                            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-                        >
-                            <option value="">Selecciona Subproducto</option>
-                            {subproductos.map((sub, index) => (
-                                <option key={index} value={sub}>
-                                    {sub}
-                                </option>
-                            ))}
-                        </select>
-                        {errors.subproducto_luz && <p className="text-red-500 text-sm">{errors.subproducto_luz[0]}</p>}
-                    </div>
-                )}
-                {/* Subproducto Gas Field */}
-                {(producto === "DUAL") && (
-                    <div>
-                        <label htmlFor="subproductoGas" className="block text-sm font-medium text-gray-700">
-                            Subproducto Gas
-                        </label>
-                        <select
-                            id="subproductoGas"
-                            value={subproductoGas}
-                            onChange={(e) => setSubproductoGas(e.target.value)}
-                            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-                        >
-                            <option value="">Selecciona Subproducto Gas</option>
-                            {subproductosGas.map((sub, index) => (
-                                <option key={index} value={sub}>
-                                    {sub}
-                                </option>
-                            ))}
-                        </select>
-                        {errors.subproducto_gas && <p className="text-red-500 text-sm">{errors.subproducto_gas[0]}</p>}
-                    </div>
-                )}
-            </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-                {/* Mantenimiento Luz Field */}
-                {(producto === "LUZ" || producto === "GAS" || producto === "DUAL") && (
-                    <div>
-                        <label htmlFor="mantenimiento" className="block text-sm font-medium text-gray-700">
-                            Mantenimiento
-                        </label>
-                        <select
-                            id="mantenimiento"
-                            value={mantenimiento}
-                            onChange={(e) => setMantenimiento(e.target.value)}
-                            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-                        >
-                            <option value="">Selecciona Mantenimiento</option>
-                            <option value="SI">SI</option>
-                            <option value="NO">NO</option>
-                        </select>
-                        {errors.mantenimiento && (
-                            <p className="text-red-500 text-sm">{errors.mantenimiento[0]}</p>
-                        )}
-                    </div>
-                )}
-                {/* Mantenimiento Gas Field */}
-                {(producto === "DUAL") && (
-                    <div>
-                        <label htmlFor="mantenimientoGas" className="block text-sm font-medium text-gray-700">
-                            Mantenimiento Gas
-                        </label>
-                        <select
-                            id="mantenimientoGas"
-                            value={mantenimientoGas}
-                            onChange={(e) => setMantenimientoGas(e.target.value)}
-                            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-                        >
-                            <option value="">Selecciona Mantenimiento Gas</option>
-                            <option value="SI">SI</option>
-                            <option value="NO">NO</option>
-                        </select>
-                        {errors.mantenimiento_gas && (
-                            <p className="text-red-500 text-sm">{errors.mantenimiento_gas[0]}</p>
-                        )}
-                    </div>
-                )}
-            </div>
+            {producto &&
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+                    {/* Subproducto Luz Field */}
+                    {(producto === "LUZ" || producto === "GAS" || producto === "DUAL") && (
+                        <div>
+                            <label htmlFor="subproducto" className="block text-sm font-medium text-gray-700">
+                                Subproducto
+                            </label>
+                            <select
+                                id="subproducto"
+                                value={subproducto}
+                                onChange={(e) => setSubproducto(e.target.value)}
+                                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                            >
+                                <option value="">Selecciona Subproducto</option>
+                                {subproductos.map((sub, index) => (
+                                    <option key={index} value={sub}>
+                                        {sub}
+                                    </option>
+                                ))}
+                            </select>
+                            {errors.subproducto_luz &&
+                                <p className="text-red-500 text-sm">{errors.subproducto_luz[0]}</p>}
+                        </div>
+                    )}
+                    {/* Subproducto Gas Field */}
+                    {(producto === "DUAL") && (
+                        <div>
+                            <label htmlFor="subproductoGas" className="block text-sm font-medium text-gray-700">
+                                Subproducto Gas
+                            </label>
+                            <select
+                                id="subproductoGas"
+                                value={subproductoGas}
+                                onChange={(e) => setSubproductoGas(e.target.value)}
+                                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                            >
+                                <option value="">Selecciona Subproducto Gas</option>
+                                {subproductosGas.map((sub, index) => (
+                                    <option key={index} value={sub}>
+                                        {sub}
+                                    </option>
+                                ))}
+                            </select>
+                            {errors.subproducto_gas &&
+                                <p className="text-red-500 text-sm">{errors.subproducto_gas[0]}</p>}
+                        </div>
+                    )}
+                </div>
+            }
+            {/* Mantenimiento */}
+            {producto &&
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+                    {/* Mantenimiento Luz */}
+                    {(producto === "LUZ" || producto === "GAS" || producto === "DUAL") && (
+                        <div>
+                            <label htmlFor="mantenimiento" className="block text-sm font-medium text-gray-700">
+                                Mantenimiento
+                            </label>
+                            <select
+                                id="mantenimiento"
+                                value={mantenimiento}
+                                onChange={(e) => setMantenimiento(e.target.value)}
+                                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                            >
+                                <option value="">Selecciona Mantenimiento</option>
+                                <option value="SI">SI</option>
+                                <option value="NO">NO</option>
+                            </select>
+                            {errors.mantenimiento && (
+                                <p className="text-red-500 text-sm">{errors.mantenimiento[0]}</p>
+                            )}
+                        </div>
+                    )}
+                    {/* Mantenimiento Gas */}
+                    {(producto === "DUAL") && (
+                        <div>
+                            <label htmlFor="mantenimientoGas" className="block text-sm font-medium text-gray-700">
+                                Mantenimiento Gas
+                            </label>
+                            <select
+                                id="mantenimientoGas"
+                                value={mantenimientoGas}
+                                onChange={(e) => setMantenimientoGas(e.target.value)}
+                                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                            >
+                                <option value="">Selecciona Mantenimiento Gas</option>
+                                <option value="SI">SI</option>
+                                <option value="NO">NO</option>
+                            </select>
+                            {errors.mantenimiento_gas && (
+                                <p className="text-red-500 text-sm">{errors.mantenimiento_gas[0]}</p>
+                            )}
+                        </div>
+                    )}
+                </div>
+            }
+            {/* Estado */}
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
                 <div>
                     <label htmlFor="estatus" className="block text-sm font-medium text-gray-700">
