c664a953f41f1eebb34ce11ead313cae2296403f
victorFernandez173 <victor.fer.gor@proton.me>
2024-12-24 00:06:54 +0100

    añadido controlador básico para el login mediante sanctum y token

---
+++ app/Http/Controllers/SanctumAuthController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Auth;
+use Illuminate\Http\Request;
+
+class SanctumAuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json(['token' => $token]);
+    }
+
+}
