f34477fd1c8d4c1d0a543cb94dbbd5caed48c56f
victorFernandez173 <victor.fer.gor@proton.me>
2025-01-28 10:17:08 +0100

    funcion de prueba modificada

---
+++ app/Http/Controllers/NewController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreEncuestaRequest;
+use App\Models\Encuesta;
+use Inertia\Inertia;
+
+class NewController extends Controller
+{
+
+    // aquí el código de NewController
+    public function nuevaFuncionEjemplo()
+    {
+        // aquí el código de la nueva función de prueba
+    }
+
+}
